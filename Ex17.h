9f5923fbbfed7b9ca90baa28c0302ccad5ba2810
IBZua <120250986+IBZua@users.noreply.github.com>
2022-12-16 13:47:12 +0100

    Add files via upload

---
+++ Ex17.h
@@ -0,0 +1,15 @@
+//
+//  Ex2.h
+//  TME3
+//
+//  Created by Bogouslavsky Illia on 09/12/2022.
+//
+
+#ifndef Ex2_h
+#define Ex2_h
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+int valeur_aleatoire(int min, int max);
+void pos_neg_zero(int val, int *pos, int *neg, int *zero);
+#endif /* Ex2_h */
